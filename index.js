53d4260b3d5edb83fb9216d10db0db6331b1716b
Rodrigohh <Rodrigokum091@gmail.com>
2023-05-30 01:04:41 -0600

    Diseño de la aplicacion con css y responsive dising junto con la funcionalidad de encriptar un texto

---
+++ index.js
@@ -0,0 +1,27 @@
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("muneco");
+
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi,"ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat")
+    
+
+    if (texto != 0 ){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito"
+        parrafo.textContent = "";
+        muneco.src = "./img/encriptado.jpg";
+    }else{
+        muneco.src = "./img/muñeco.png"
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        alert("Debes ingresar algun texto");
+    }
+
+}
